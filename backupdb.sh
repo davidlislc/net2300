ab951ad97aa7faf21433f77ce9c3a3402ff64ec4
ethandominy03-max <ethandominy03@gmail.com>
2026-04-22 17:31:22 -0400

    Refactor backupdb.sh for improved security
    Removed configuration comments and masked password for security.

--- backupdb.sh
+++ backupdb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Configuration
 CONTAINER="docker-compose-db-1"
 DB_USER="root"
 DB_PASS="somewordpress"
@@ -8,13 +7,11 @@ DB_NAME="wordpress"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 BACKUP_FILE="backup_${TIMESTAMP}.sql"
 
-# Masked Password for log security
 MASKED_PASS="**********"
 
 echo "Starting automated backup for database: ${DB_NAME}..."
 echo "Using password: ${MASKED_PASS}"
 
-# Execute the dump via Docker container
 docker exec $CONTAINER mysqldump -u $DB_USER -p$DB_PASS $DB_NAME > "$BACKUP_FILE"
 
 if [ $? -eq 0 ]; then
